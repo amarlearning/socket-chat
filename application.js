ba93ab2fc23b23974e1963df6fb24df2a3aa59e6
Amar Prakash Pandey <amarlearning@users.noreply.github.com>
2016-07-15 00:17:49 +0530

    Delete application.js

--- application.js
+++
@@ -1,9 +0,0 @@
-// working on camera module
-navigator.webkitGetUserMedia({video: true},
-  function(stream) {
-    document.getElementById('video').src = URL.createObjectURL(stream);
-  },
-  function(error) {
-    // error code 
-  }
-);
